b1dffc7bcb240af7e5f48775756d9744c14f7da5
AsakaJX <yourasaka@protonmail.com>
2025-07-07 00:15:08 +0300

    Add Somewhat working game logic

--- main.js
+++ main.js
@@ -1,106 +1,130 @@
-// function getUserChoice() {
-//   let userChoice = '';
+function getUserChoice(event) {
+  const btnId = String(event.target.id);
+  const choice = btnId.substring(btnId.indexOf('-') + 1);
 
-//   while (true) {
-//     userChoice = prompt('Enter your choice: ', '');
+  return choice;
+}
 
-//     if (
-//       userChoice === 'rock' ||
-//       userChoice === 'paper' ||
-//       userChoice === 'scissors'
-//     ) {
-//       break;
-//     }
+function getComputerChoice() {
+  let computerChoice = Math.floor(Math.random() * 3) + 1;
 
-//     console.log('incorrect input!\n\n');
-//   }
+  return computerChoice == 1
+    ? 'rock'
+    : computerChoice == 2
+    ? 'paper'
+    : 'scissors';
+}
 
-//   return userChoice;
-// }
+function isUserWinning(userChoice, computerChoice) {
+  if (userChoice === 'rock') {
+    return computerChoice === 'scissors' ? true : false;
+  } else if (userChoice === 'paper') {
+    return computerChoice === 'rock' ? true : false;
+  } else {
+    return computerChoice === 'paper' ? true : false;
+  }
+}
 
-// function getComputerChoice() {
-//   let computerChoice = Math.floor(Math.random() * 3) + 1;
+function playRound(userChoice, computerChoice) {
+  let result = 'tie';
 
-//   return computerChoice == 1
-//     ? 'rock'
-//     : computerChoice == 2
-//     ? 'paper'
-//     : 'scissors';
-// }
+  if (userChoice !== computerChoice) {
+    result = isUserWinning(userChoice, computerChoice) === true ? 'user' : 'ai';
+  }
 
-// function isUserWinning(userChoice, computerChoice) {
-//   if (userChoice === 'rock') {
-//     return computerChoice === 'scissors' ? true : false;
-//   } else if (userChoice === 'paper') {
-//     return computerChoice === 'rock' ? true : false;
-//   } else {
-//     return computerChoice === 'paper' ? true : false;
-//   }
-// }
+  return result;
+}
 
-// function playRound(userChoice, computerChoice) {
-//   let result = 'tie';
+let userScore = +document.querySelector('game-score-user');
+let computerScore = +document.querySelector('game-score-computer');
 
-//   if (userChoice !== computerChoice) {
-//     result =
-//       isUserWinning(userChoice, computerChoice) === true ? 'user' : 'computer';
-//   }
+const choiceButton = document.querySelector('.user-input-container');
+choiceButton.addEventListener('click', playGame);
 
-//   return result;
-// }
+let roundCount = 0;
 
-// let userScore = 0;
-// let computerScore = 0;
+let gameWinner = '';
 
-// function playGame() {
-//   for (let currentRound = 1; currentRound <= 5; currentRound++) {
-//     let userChoice = getUserChoice();
-//     let computerChoice = getComputerChoice();
+function resetGame() {
+  gameWinner = '';
+  roundCount = 0;
+  computerScore = 0;
+  userScore = 0;
+}
 
-//     let roundWinner = playRound(userChoice, computerChoice);
+function playGame(userChoiceButtonEvent) {
+  roundCount++;
 
-//     // Track score
+  let userChoice = getUserChoice(userChoiceButtonEvent);
+  let computerChoice = getComputerChoice();
 
-//     if (roundWinner === 'user') {
-//       userScore++;
-//     } else if (roundWinner === 'computer') {
-//       computerScore++;
-//     } else {
-//       // if we want to add to both scores
-//       // userScore++;
-//       // computerScore++;
-//     }
+  let roundWinner = playRound(userChoice, computerChoice);
 
-//     // Displaying info
-//     displayRoundInfo(currentRound, roundWinner, userChoice, computerChoice);
-//   }
+  // Track score
 
-//   return userScore === computerScore
-//     ? 'tie'
-//     : userScore > computerScore
-//     ? 'user'
-//     : 'computer';
-// }
+  if (roundWinner === 'user') {
+    userScore++;
+  } else if (roundWinner === 'ai') {
+    computerScore++;
+  } else {
+    // if we want to add to both scores
+    // userScore++;
+    // computerScore++;
+  }
 
-// function displayRoundInfo(currentRound, winner, userChoice, computerChoice) {
-//   let winnerMessage =
-//     winner === 'user' ? `beats` : winner === 'computer' ? `loses to` : 'equals';
+  // Had to move the gameWinner dicision here, it's not really right place to do so,
+  // but it doesn't messes up with the final product, so... Imma leave it for now.
+  //
+  // The problem was I was assigning it after the displayRoundInfo was called,
+  // so there was no information about the gameWinner at that point.
+  gameWinner =
+    userScore === computerScore
+      ? 'tie'
+      : userScore > computerScore
+      ? 'user'
+      : 'ai';
 
-//   console.log(
-//     `Round #${currentRound} < Score: ${userScore} - ${computerScore} >`
-//   );
+  // Displaying info
+  displayRoundInfo(roundWinner);
 
-//   console.log(
-//     'Choices -> user: ' + userChoice + ' computer: ' + computerChoice
-//   );
+  if (roundCount === 5) {
+    resetGame();
+  }
+}
 
-//   console.log(
-//     `${winner} ${
-//       winner !== 'tie' ? 'wins ' : ''
-//     }! [${userChoice} ${winnerMessage} ${computerChoice}]` + '\n\n'
-//   );
-// }
+function displayRoundInfo(roundWinner) {
+  const userCounter = document.querySelector('#game-score-user');
+  const computerCounter = document.querySelector('#game-score-computer');
 
-// let gameResult = playGame();
+  userCounter.textContent = userScore;
+  computerCounter.textContent = computerScore;
 
-// console.log(`\n\n\nGame result: ${gameResult}\n\n\n`);
+  const userCard = document.querySelector('.game-choice-user');
+  const computerCard = document.querySelector('.game-choice-computer');
+
+  const resultPlaceholder = document.querySelector('.result-placeholder');
+
+  const resultText =
+    roundCount !== 5
+      ? `Round winner: ${roundWinner}`
+      : `Game winner: ${gameWinner}`;
+
+  resultPlaceholder.textContent = resultText;
+
+  switch (roundWinner) {
+    case 'user':
+      userCard.style.border = `2px dotted oklch(0.7 0.06 160)`;
+      computerCard.style.border = ``;
+      break;
+
+    case 'ai':
+      computerCard.style.border = `2px dotted oklch(0.7 0.06 160)`;
+      userCard.style.border = ``;
+      break;
+
+    default:
+      computerCard.style.border = `2px dotted oklch(0.56 0.1 296)`;
+      userCard.style.border = `2px dotted oklch(0.56 0.1 296)`;
+      break;
+  }
+}
